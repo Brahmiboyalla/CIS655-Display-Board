e0c0b8493fa6061a5a9e4c3d8962d719521876e2
Brahmiboyalla <144056799+Brahmiboyalla@users.noreply.github.com>
2024-04-14 21:28:37 -0400

    Update App.js

--- Display-board/frontend/src/App.js
+++ Display-board/frontend/src/App.js
@@ -1,11 +1,15 @@
 import Navigation from "./components/Navigation"
 import DisplayPosts from "./components/DisplayPosts"
 import AddPost from "./components/AddPost"
+
+import { useState } from "react";
+
 function App() {
+  const [changes,setChanges] = useState(false);
   return (<>
     <Navigation />
-    <AddPost />
-    <DisplayPosts />
+    <AddPost changes={changes} setChanges={setChanges}/>
+    <DisplayPosts changes={changes} setChanges={setChanges}/>
   </>
   );
 }
